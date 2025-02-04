fd0a34cd8a8021fd415a74f4b00e50c040a66213
Abnaum89 <toratti.abner@gmail.com>
2025-02-03 21:49:38 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+//let nome = 'Abner'
+//console.log(`Boas vindas ${nome}!`);
+//alert(`Olá ${nome}!`)
+//let lingProgFavorita = prompt('Qual sua linguagem de programação favorita?')
+//console.log(lingProgFavorita)
+
+//let valor1 = 5
+//let valor2 = 10
+//let resultado = valor1 + valor2
+//console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`)
+
+//let idade = prompt('Qual a sua idade?');
+
+//if (idade > 17) {
+ //   alert('Vc é maior de idade :)')
+//} else {
+ //   alert('Vc é menor de idade :(')
+//}
+
+let numero = 1
+
+while (numero <= 10) {
+    console.log(numero);
+    numero++
+}
+
+let nota = 6
+
+if(nota >= 7) {
+    console.log('Aprovado')
+} else {
+    console.log('Reprovado')
+}
+
+let numeroAleatorio = parseInt(Math.random() * 1000 + 1)
+console.log(numeroAleatorio)
